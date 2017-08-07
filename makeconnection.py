441d85660477ba9cb84f627f670aeee80ec6a1d2
Daniel <none@localhost>
2017-08-07 21:05:40 +0200

    Small modification with a lot of test but with no solution.

--- makeconnection.py
+++ makeconnection.py
@@ -1,7 +1,8 @@
 import pymodbus
 import serial
 from pymodbus.pdu import ModbusRequest
-from pymodbus.client.sync import ModbusSerialClient as ModbusClient #initialize a serial RTU client instance
+#initialize a serial RTU client instance
+from pymodbus.client.sync import ModbusSerialClient as ModbusClient
 from pymodbus.transaction import ModbusRtuFramer
 
 import logging
@@ -13,16 +14,16 @@ log.setLevel(logging.DEBUG)
 #unit= the slave unit this request is targeting
 #address= the starting address to read from
 
-client= ModbusClient(method = "rtu", port="/dev/ttyUSB0",stopbits = 1, bytesize = 8, parity = 'E' baudrate= 9600)
+client= ModbusClient(method = "rtu", port="/dev/ttyUSB0",timeout=0.5,stopbits = 1, bytesize = 8,baudrate=19200)
 
 #Connect to the serial modbus server
 connection = client.connect()
 print connection
 
 #Starting add, num of reg to read, slave unit.
-result= client.read_holding_registers(0x00,2,unit= 0xff)
+#result= client.read_holding_registers(0x1B58,2)
+result= client.read_holding_registers(0x1B58,2,unit=1)
 
-print(result)
-
-#Closes the underlying socket connection
+print "AAAAAAAAAaa: %s" %(result)
+#closes the underlying socket connection
 client.close()
